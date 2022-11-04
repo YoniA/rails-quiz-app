a10f6c358aaaed2d1edf9bcda7b0bc24723d68ea
yoni <yoni@yonis-MacBook-Air.local>
2022-11-04 21:13:33 +0200

    Add validators on User and Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  validates :body, :ans1, :ans2, :ans3, :ans4, :key, presence: true
+  validates :key, inclusion: { in: %w(a b c d), message: "Illegal key" }
 end
